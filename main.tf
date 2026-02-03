640707bddd0e58921641c702d239f9f278caf01c
Aniket Pawar <712aniket@gmail.com>
2026-02-03 14:40:08 +0000

    hard reset

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "ubuntu_vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Leaving this empty gives the VM a Public IP
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" { type = string }
+variable "instance_name" { type = string }
+variable "region" { type = string }
+variable "zone" { type = string }
+variable "machine_type" { type = string }
